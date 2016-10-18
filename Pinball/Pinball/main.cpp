782a9eb9b621b647a8c330bf1ef872a5efae94da
AlfonsoSanchez <alfonsosp2@tr12.cat>
2016-10-18 16:02:25 +0200

    All headers ,we need

---
+++ Pinball/Pinball/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+enum main_states
+{
+	MAIN_CREATION,
+	MAIN_START,
+	MAIN_UPDATE,
+	MAIN_FINISH,
+	MAIN_EXIT
+};
+
+int main()
+{
+	main_states state = MAIN_CREATION;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
