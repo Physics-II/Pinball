452f5b72884bb9a3937deae0673371b37bdc8d66
AlfonsoSanchez <alfonsosp2@tr12.cat>
2016-10-18 17:55:40 +0200

    background

---
+++ Pinball/ModuleTextures.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+#include "p2List.h"
+#include "SDL\include\SDL.h"
+
+class ModuleTextures : public Module
+{
+public:
+	ModuleTextures(Application* app, bool start_enabled = true);
+	~ModuleTextures();
+
+	SDL_Texture* graphics = nullptr;
+
+	bool Init();
+	bool CleanUp();
+
+	SDL_Texture* const Load(const char* path);
+	void Unload(SDL_Texture* texture);
+
+public:
+	p2List<SDL_Texture*> textures;
+};
